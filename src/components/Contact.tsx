6585579c0ba0fddba5c36dbf4cb5d31a839003b6
Matt Rigg <matt@mattrigg.dev>
2023-11-27 19:54:33 -0800

    Add initial Contact and Footer component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,85 @@
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section className="min-h-screen">
+      <StaticImage
+        src="../images/contact-bg.jpg"
+        alt="Landscape with boats"
+        className=" absolute -z-1 object-cover object-top-right h-full"
+      />
+      <div className="isolate px-6 py-24 sm:py-32 lg:px-8">
+        <header className="mx-auto max-w-2xl text-center">
+          <p className="text-base font-semibold leading-7 text-primary">Want to work together?</p>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Let's talk
+          </h2>
+        </header>
+        <form
+          action="#"
+          method="POST"
+          className="mx-auto mt-16 max-w-xl p-8 rounded-xl backdrop-blur-md backdrop-brightness-[1.2] bg-white/30"
+        >
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label htmlFor="name" className="block text-sm font-semibold leading-6 text-gray-900">
+                Name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  autoComplete="name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 transition-shadow"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 transition-shadow"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6 transition-shadow"
+                  defaultValue={""}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Let's talk
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
