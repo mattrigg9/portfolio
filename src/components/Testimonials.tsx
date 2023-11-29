18933cb98d0591334662b858cbd56d99384be9cd
Matt Rigg <matt@mattrigg.dev>
2023-11-29 11:37:47 -0800

    Add headshots to testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,67 +1,108 @@
-import React from "react";
+import React, { useMemo } from "react";
 import classNames from "../utils/classNames";
-
-const featuredTestimonial = {
-  body: "Matt is regarded as a pillar of our team. Working tirelessly and without ego, Matt is a rare breed in this industry. His eye for design and user experience shines through in tools that he passionately creates. He is constantly coming up with ideas and mechanisms that I never knew I wanted until I got them.",
-  author: {
-    name: "Ian Webb",
-    title: "Senior Engineer, Amazon",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    logoUrl: "",
-  },
-};
-
-const testimonials = [
-  [
-    [
-      {
-        body: "Collaborating with Matt has been a privilege. His unwavering dedication, creative problem-solving, and impeccable design sense have been remarkable. Matt's contributions have significantly shaped CO2.com's success, making it both technically robust and user-friendly. I'm confident that his expertise will continue to drive positive changes in any project he's a part of.",
-        author: {
-          name: "Nathan Fixler",
-          title: "Senior Engineer, TIME",
-          imageUrl:
-            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      },
-    ],
-  ],
-  [
-    [
-      {
-        body: "Matt is a talented engineer who is excellent at dealing with ambiguity. Though what sets him apart is his ability to don the various hats as needed to deliver on any given project: UX Designer, Researcher, or Product Manager, in addition to engineering great customer facing software solutions. Matt's customer empathy, team engagement, and strong design and technical skills enables him to work directly with customers to understand their key problems. Matt is a great asset for any solution.",
-        author: {
-          name: "Trevor Gruby",
-          title: "Principal Engineering Manager, Microsoft",
-          imageUrl:
-            "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      },
-    ],
-    [
-      {
-        body: "Matt has a super-human ability to build and deliver features. He routinely completes tasks much faster than I would expect. He is excellent at building trust with others and is quick to speak up and provide praise when warranted. Matt cares deeply about his customers, whether internal or external, and he always strives to improve their experience.",
-        author: {
-          name: "Fernando Hernandez",
-          title: "Front End Engineer, Amazon",
-          imageUrl:
-            "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-        },
-      },
-    ],
-  ],
-];
+import { GatsbyImage, StaticImage } from "gatsby-plugin-image";
+import { graphql, useStaticQuery } from "gatsby";
 
 export default function Testimonials() {
+  // Images cannot be passed to StaticImage as variables, so they must be queried to be managed in an array.
+  const data = useStaticQuery(graphql`
+    {
+      amazonLogo: file(relativePath: { eq: "amazon-logo.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 120, quality: 90, placeholder: NONE)
+        }
+      }
+      placeholder: file(relativePath: { eq: "headshots/placeholder-male.png" }) {
+        childImageSharp {
+          gatsbyImageData(width: 75, height: 75, quality: 90, aspectRatio: 1)
+        }
+      }
+      ian: file(relativePath: { eq: "headshots/ian-webb.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(width: 75, height: 75, quality: 90)
+        }
+      }
+      trevor: file(relativePath: { eq: "headshots/trevor-gruby.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(width: 75, height: 75, quality: 90)
+        }
+      }
+      nathan: file(relativePath: { eq: "headshots/nathan-fixler.jpg" }) {
+        childImageSharp {
+          gatsbyImageData(width: 75, height: 75, quality: 90)
+        }
+      }
+    }
+  `);
+
+  console.log(data);
+
+  const featuredTestimonial = useMemo(
+    () => ({
+      body: "Matt is regarded as a pillar of our team. Working tirelessly and without ego, Matt is a rare breed in this industry. His eye for design and user experience shines through in tools that he passionately creates. He is constantly coming up with ideas and mechanisms that I never knew I wanted until I got them.",
+      author: {
+        name: "Ian Webb",
+        title: "Senior Engineer, Amazon",
+        image: data.ian.childImageSharp.gatsbyImageData,
+        companyLogo: data.amazonLogo.childImageSharp.gatsbyImageData,
+        companyName: "Amazon",
+      },
+    }),
+    [],
+  );
+
+  const testimonials = useMemo(
+    () => [
+      [
+        [
+          {
+            body: "Collaborating with Matt has been a privilege. His unwavering dedication, creative problem-solving, and impeccable design sense have been remarkable. Matt's contributions have significantly shaped CO2.com's success, making it both technically robust and user-friendly. I'm confident that his expertise will continue to drive positive changes in any project he's a part of.",
+            author: {
+              name: "Nathan Fixler",
+              title: "Senior Engineer, TIME",
+              image: data.nathan.childImageSharp.gatsbyImageData,
+            },
+          },
+        ],
+      ],
+      [
+        [
+          {
+            body: "Matt is a talented engineer who is excellent at dealing with ambiguity. Though what sets him apart is his ability to don the various hats as needed to deliver on any given project: UX Designer, Researcher, or Product Manager, in addition to engineering great customer facing software solutions. Matt's customer empathy, team engagement, and strong design and technical skills enables him to work directly with customers to understand their key problems. Matt is a great asset for any solution.",
+            author: {
+              name: "Trevor Gruby",
+              title: "Principal Engineering Manager, Microsoft",
+              image: data.trevor.childImageSharp.gatsbyImageData,
+            },
+          },
+        ],
+        [
+          {
+            body: "Matt has a super-human ability to build and deliver features. He routinely completes tasks much faster than I would expect. He is excellent at building trust with others and is quick to speak up and provide praise when warranted. Matt cares deeply about his customers, whether internal or external, and he always strives to improve their experience.",
+            author: {
+              name: "Fernando Hernandez",
+              title: "Front End Engineer, Amazon",
+              image: data.placeholder.childImageSharp.gatsbyImageData,
+            },
+          },
+        ],
+      ],
+    ],
+    [],
+  );
+
   return (
-    <section
-      className="relative isolate pb-32 pt-24 sm:pt-32 bg-[conic-gradient(at_top_right,_var(--tw-gradient-stops))] from-fuchsia-500 via-emerald-200 to-gray-100"
-      id="testimonials"
-    >
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+    <section className="min-h-screen relative" id="testimonials">
+      <StaticImage
+        src="../images/belay.jpg"
+        alt="Mountaineer on belay"
+        className="absolute -z-10 h-full"
+        imgClassName="lg:object-right-top"
+      />
+      <div className="mx-auto max-w-7xl px-6 lg:px-8 isolate pb-32 pt-24 sm:pt-32">
         <header className="mx-auto max-w-xl text-center">
-          <p className="text-base font-semibold leading-7 text-primary">Build confidently</p>
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <p className="text-base font-semibold leading-7 text-white">Build confidently</p>
+          <h2 className="text-3xl font-bold tracking-tight text-gray-100 sm:text-4xl">
             Testimonials
           </h2>
         </header>
@@ -74,20 +115,22 @@ export default function Testimonials() {
               </p>
             </blockquote>
             <figcaption className="flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
-              <img
-                className="h-10 w-10 flex-none rounded-full bg-gray-50"
-                src={featuredTestimonial.author.imageUrl}
-                alt=""
+              <GatsbyImage
+                className="h-10 w-10 rounded-full bg-gray-50"
+                image={featuredTestimonial.author.image}
+                alt={featuredTestimonial.author.name}
               />
+
               <div className="flex-auto">
                 <div className="font-semibold">{featuredTestimonial.author.name}</div>
                 <div className="text-gray-600">{featuredTestimonial.author.title}</div>
               </div>
-              <img
-                className="h-10 w-auto flex-none"
-                src={featuredTestimonial.author.logoUrl}
-                alt=""
-              />
+              {featuredTestimonial.author.companyLogo && (
+                <GatsbyImage
+                  image={featuredTestimonial.author.companyLogo}
+                  alt={featuredTestimonial.author.companyName}
+                />
+              )}
             </figcaption>
           </figure>
           {testimonials.map((columnGroup, columnGroupIdx) => (
@@ -113,11 +156,15 @@ export default function Testimonials() {
                         <p>{`“${testimonial.body}”`}</p>
                       </blockquote>
                       <figcaption className="mt-6 flex items-center gap-x-4">
-                        <img
-                          className="h-10 w-10 rounded-full bg-gray-50"
-                          src={testimonial.author.imageUrl}
-                          alt=""
-                        />
+                        {testimonial.author.image && (
+                          <GatsbyImage
+                            className="h-10 w-10 rounded-full bg-gray-50"
+                            image={testimonial.author.image}
+                            alt={testimonial.author.name}
+                            objectFit="cover"
+                            imgClassName="object-left-center"
+                          />
+                        )}
                         <div>
                           <div className="font-semibold leading-7">{testimonial.author.name}</div>
                           <div className="text-gray-600 leading-4">{testimonial.author.title}</div>
